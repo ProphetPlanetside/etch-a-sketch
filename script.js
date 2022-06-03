3c16f7e82a25df733b87178f7a6720628875c6d4
Jay Spendlove <jayaloha@hotmail.com>
2022-06-03 09:34:28 -0700

    Added code to css and js files, now the program loads a grid of 16 (4x4) squares.

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const grid = document.querySelector('#grid');
+// This piece of code works as a 'hover' eventListener, as it kicks off the event when you 'mouse over' the element.
+// grid.addEventListener("mouseover", function() {message()});
 
-let n = 2;
+function changeColor(square) {
+    // alert("Hello");
+    square.style.cssText = "background-color: red;"
+}
+
+let n = 4;
 
 // the backtick ` let's me use a variable in my JS script for this line below.
 grid.style.cssText = `grid-template-columns: repeat(${n}, 1fr);`
 
 function createSquare() {
-    const square = document.createElement('div');
-    square.classList.add('test');
-    square.textContent= "Test";
+    const square = document.createElement('square');
+    square.classList.add('square');
+    // square.textContent= "Square";
 
     grid.appendChild(square);
-}
 
-for (i = 0; i < 5; i++) {
+    square.addEventListener("mouseover", function() {changeColor(square)});
+ }
+
+// Creates squares equal to n squared
+for (i = 0; i < n*n; i++) {
     createSquare();
 }
+
+// grid.querySelectorAll(':scope > square').addEventListener("mouseover", function() {message()});
+
+//const squares = grid.querySelectorAll(':scope > square');
+
+// squares.addEventListener("mouseover", function() {message()});
