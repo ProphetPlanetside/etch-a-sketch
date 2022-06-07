0bd4a1740b68532d9c7856aa1a6df756b8613589
Jay Spendlove <jayaloha@hotmail.com>
2022-06-07 11:22:02 -0700

    Program is working in a draft-state, but only with 10x10 grid

--- script.js
+++ script.js
@@ -2,27 +2,31 @@ const grid = document.querySelector('#grid');
 // This piece of code works as a 'hover' eventListener, as it kicks off the event when you 'mouse over' the element.
 // grid.addEventListener("mouseover", function() {message()});
 
+// Function changes the color of the square to red when called.
 function changeColor(square) {
-    // alert("Hello");
-    square.style.cssText = "background-color: red;"
+    // square.style.cssText = "background-color: red;"
+    square.style.backgroundColor = 'red';
 }
 
+// n = number of rows/columns. For a 16x16 grid, n=16.
 let n = 10;
-
-// the backtick ` let's me use a variable in my JS script for this line below.
-grid.style.cssText = `grid-template-columns: repeat(${n}, 1fr);`
 
 function createSquare() {
     const square = document.createElement('square');
     square.classList.add('square');
     
-    // This is the calculation to find the flex-basis, maybe I should use a different
-    // name other than "width" here though.
-    const width = 100/n;
-    square.style.cssText = `flex-basis: ${width}%;`
+    // This is the calculation to find the flex-basis, it's not applying to the css.
+    const flexBasis = 100/n;
+    
+    square.style.flexBasis = '10%';
+    //square.style.cssText = `flex-basis: ${flexBasis}%;`
+
     // Height calculation below is wrong. It works when n=10 but not for other numbers.
-    const height = n*80/10;
-    square.style.cssText = `height: ${height}px;`
+    // Height of the grid must be 800, so the combined height of all squares must equal 800.
+    // So this value should equal 800 / n.
+    const height = 800 / n;
+    //square.style.cssText = `height: 80px;`
+    //square.style.cssText = `height: ${height}px;`
 
     grid.appendChild(square);
     square.addEventListener("mouseover", function() {changeColor(square)});
