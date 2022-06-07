4f7a88045ab5915ce731b5a639b4a5eba6c23562
Jay Spendlove <jayaloha@hotmail.com>
2022-06-07 12:43:32 -0700

    Completed adding features. The grid is intially 16x16. You can now click a button that will prompt you for a number greater than 0 and less than 101, and a new grid will be created in the same pixel space of nxn, where n is the number you entered.

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-const grid = document.querySelector('#grid');
-
 // Function changes the color of the square to red when called.
 function changeColor(square) {
-    // square.style.cssText = "background-color: red;"
     square.style.backgroundColor = 'red';
 }
 
-// n = number of rows/columns. For a 16x16 grid, n=16.
-let n = 4;
-
-function createSquare() {
+// n = number of rows/columns. i.e. For a 16x16 grid, n = 16.
+function createSquare(n) {
+    // Create a new element called "square".
     const square = document.createElement('square');
+    // Add the "square" class to this new variable from the CSS.
     square.classList.add('square');
     
-    // This is the calculation to find the flex-basis, it's not applying to the css.
+    // This is the calculation to find the flex-basis. Flex basis is the percentage of the row-space
+    // that the square will take up.  i.e. if it equals 25%, then 4 squares will fit in a row.
     const flexBasis = 100/n;
     
+    // Add the css style "flex" to the square. This was required before being able to do the next line of code.
     square.style.display = "flex";
-    //square.style.flexBasis = '10%';
+    // Set the flex-basis value to be equal to the value we just found above from 100/n.
     square.style.cssText = `flex-basis: ${flexBasis}%;`
-    // square.style.height = "80px";
 
     // Height calculation below works. Total height / num of squares equals individual square height.
     const heightValue = 800 / n;
@@ -33,8 +31,58 @@ function createSquare() {
     square.addEventListener("mouseover", function() {changeColor(square)});
  }
 
-// Creates squares equal to n squared
-for (i = 0; i < n*n; i++) {
-    createSquare();
+// n = number of rows/columns. i.e. For a 16x16 grid, n = 16.
+function etchASketch(n) {
+    // Match a new variable "grid" to the div with ID 'grid' from the HTML file.
+    const grid = document.querySelector('#grid');
+
+    // Clear the grid.
+    removeAllChildNodes(grid);
+
+    // Creates squares equal to n squared
+    for (i = 0; i < n*n; i++) {
+        createSquare(n);
+    }
 }
 
+// This function removes all child nodes from an element (removes all squares on the grid in this case).
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// Makes sure the new grid meets requirements before creation.
+function newGridPrompt() {
+    let answer = 101;
+    while (answer > 100) {
+        answer = prompt("How many squares per side would you like? (Max: 100)");
+        // If the answer is not a number.
+        if(isNaN(answer)) {
+            alert("That is not a number; please enter a number.");
+            // Makes sure the while loop will continue.
+            answer = 101;
+        }
+        // If the answer is less than 1.
+        else if(answer < 1) {
+            alert("Please enter a number greater than 0, but less than 101.");
+            answer = 101;
+        }
+        // If the answer has a decimal.
+        else if(answer % 1 != 0) {
+            alert("No decimal values are allowed.");
+            answer = 101;
+        }
+    }
+    // Create a new grid.
+    etchASketch(answer);
+}
+
+// Create the initial 16x16 grid.
+etchASketch(16);
+
+// Create a variable to match to the button in the HTML.
+const button = document.querySelector('#topButton');
+// When the button is clicked, prompt the user for a number value, then create a new grid with that value of
+// squares per row.
+button.addEventListener("click", function() {newGridPrompt()});
